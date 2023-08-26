bea1ce2c6e8e332541d54337336b7ea2ebbf21a1
Dennis Pilco <39228294+dennisyaj@users.noreply.github.com>
2023-08-25 23:32:01 -0500

    Update Variables.kt

--- app/src/main/java/com/moncayo/pilco/anisham/utils/Variables.kt
+++ app/src/main/java/com/moncayo/pilco/anisham/utils/Variables.kt
@@ -1,6 +1,6 @@
 package com.moncayo.pilco.anisham.utils
 
 object Variables {
-    const val ClientID = "4074eded95db5e6743c821b860c90f39";
+    const val ClientID = "";
     const val contenidoNSFW = false
 }
